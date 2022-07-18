30e67a79fdd5063d668899db96009745661d7b25
PolichF1 <polichsegg@gmail.com>
2022-07-18 21:37:24 +0300

    fix: follow system theme at start

--- app/src/main/java/com/example/financemanager/FinanceManagerApp.kt
+++ app/src/main/java/com/example/financemanager/FinanceManagerApp.kt
@@ -1,7 +1,27 @@
 package com.example.financemanager
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import com.example.financemanager.utils.Utils
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FinanceManagerApp : Application()
+class FinanceManagerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val theme = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+            .getString(Utils.THEME_PREFERENCE_KEY, "default")
+
+        AppCompatDelegate.setDefaultNightMode(
+            when (theme) {
+                Utils.THEME_LIGHT -> AppCompatDelegate.MODE_NIGHT_NO
+                Utils.THEME_DARK -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+        )
+    }
+
+}
